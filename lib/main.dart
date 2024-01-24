4155144cdb30a03bffc5fdb4d717aa42f72c225b
shayekh <shayekhebnemizan@gmail.com>
2024-01-25 01:08:49 +0600

    google-fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: GoogleFonts.russoOneTextTheme(),
       ),
       home: const GamePage(),
     );
@@ -40,14 +43,17 @@ class _GamePageState extends State<GamePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Mega Roll"),
+        title: const Text("Mega Roll"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text('My Dicee Game',
+                style: TextStyle(
+                  fontSize: 40,
+                )),
             Image.asset('images/d5.png'),
-
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
